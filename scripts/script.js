b937fbfe9cf1950f28f1560e6043294ab84e09bc
delta_kilo <deltakilone@hotmail.com>
2024-11-08 21:23:03 +0530

    Added a simple overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-window.onscroll = function () {
-  var header = document.getElementById("header");
-  var middleText = document.getElementById("middleText");
-  var sticky = middleText.offsetTop;
+// window.onscroll = function () {
+//   var header = document.getElementById("header");
+//   var middleText = document.getElementById("middleText");
+//   var sticky = middleText.offsetTop;
 
-  if (window.pageYOffset > sticky) {
-    middleText.classList.add("sticky");
-  } else {
-    middleText.classList.remove("sticky");
-  }
-};
+//   if (window.pageYOffset > sticky) {
+//     middleText.classList.add("sticky");
+//   } else {
+//     middleText.classList.remove("sticky");
+//   }
+// };
